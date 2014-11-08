1907e41c92ad88cdf93e30d554e21bf4db48e2cd
zaighumrajput <zaighum47@gmail.com>
2014-11-08 12:45:31 +0100

    everything is inherited from type object

---
+++ JumpStartCSharpMVA/introExamples/introExamples/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace introExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///Everything is inherited from type "Object"
+            var adam = new Object();
+
+            Console.Write(adam.ToString());
+            Console.Write(adam.GetType());
+            Console.Write(adam.GetHashCode());
+
+            Console.Read();
+            
+        }
+    }
+}
