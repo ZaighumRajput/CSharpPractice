6ab84b46b1ea5a3c1349de6a10389980b6863bf9
zaighumrajput <zaighum47@gmail.com>
2014-11-09 09:58:01 +0100

    classes and struct syntax

---
+++ JumpStartCSharpMVA/classesAndStructs/classesAndStructs/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classesAndStructs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        /// <summary>
+        /// Syntax of a struct
+        /// A struct is a value type
+        /// 
+        /// Immutable; values don't change
+        /// 
+        /// Logically represents a single value
+        /// Can have a performance benefits
+        /// </summary>
+        public struct Point
+        {
+            public int X { get; set; }
+
+            public int Y { get; set; }
+        }
+        /// <summary>
+        /// Syntax of a class
+        /// 
+        /// Is a reference type
+        /// 
+        /// "Static" => cannot be instantiated
+        /// "Abstract" => cannot be instantiated; incomplete class; must be completed by a derived class
+        /// "Sealed" =>
+        /// </summary>
+        public class Animal
+        {
+
+        }
+
+    }
+}
