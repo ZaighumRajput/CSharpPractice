daaa488e620c8ae4abbb9a3c71c76ebf1bc07a28
zaighumrajput <zaighum47@gmail.com>
2014-11-13 10:17:12 +0100

    database and entity

---
+++ JumpStartCSharpMVA/databaseAndLinq/databaseAndLinq/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace databaseAndLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new Database1Entities();
+
+            var products = context.Products;
+
+            foreach (var product in products)
+                Console.WriteLine(product.Name);
+
+
+            Console.Read();
+
+        }
+    }
+}
