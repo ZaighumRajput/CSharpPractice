5205cb4f1bda41b84e98b837b9deec85937d5cd0
zaighumrajput <zaighum47@gmail.com>
2014-11-17 13:00:37 +0100

    fun with files

---
+++ JumpStartCSharpMVA/fileSystem/fileSystem/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace fileSystem
+{
+    class Program
+    {
+        /// <summary>
+        /// Illustrates interacting with the filesystem
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            //Special folders
+            var docs = Environment.SpecialFolder.MyDocuments;
+            var app = Environment.SpecialFolder.CommonApplicationData;
+            var prog = Environment.SpecialFolder.ProgramFiles;
+            var desk = Environment.SpecialFolder.Desktop;
+
+
+            foreach(var a in Enumerable.Range(0,12))
+            {
+                var filenameee = "File" + a + ".txt";
+                System.IO.File.WriteAllText(System.IO.Path.Combine(Environment.GetFolderPath(desk), filenameee), a.ToString() );
+
+            }
+
+
+            //example1();
+
+
+        }
+
+        private static void example1()
+        {
+            var dir = System.IO.Directory.GetCurrentDirectory();
+            var file = System.IO.Path.Combine(dir, "File.txt");
+            var content = "How now brown cow?";
+            System.IO.File.WriteAllText(file, content); //simplest way
+            var read = System.IO.File.ReadAllText(file);
+        }
+    }
+}
