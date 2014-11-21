215c6c5b4f86c997e0e9389c351d3d44fee8ba34
zaighumrajput <zaighum47@gmail.com>
2014-11-21 11:36:32 +0100

    really didn't quite get this one

---
+++ JumpStartCSharpMVA/webServices/webServices/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace webServices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = new Uri("http://localhost:1234/MyService.svc/json/4");
+            var client = new System.Net.WebClient();
+            var json = await client.DownloadStringTaskAsync(url);
+
+            var serializer = new JavaScriptSerializer();
+            
+        }
+    }
+}
