5c5c0801cae42e6a7e4f6e4000f78ee31b48f23d
zaighumrajput <zaighum47@gmail.com>
2014-11-23 11:38:45 +0100

    struct vs class point

---
+++ CsharpQuestionsExplained/structVSClass/structVSClass/structVSClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace structVSClass
+{
+    /// <summary>
+    /// A STRUCT that represents a point
+    /// </summary>
+    public struct PointStruct
+    {
+        public int x;
+        public int y;
+
+        //constructor
+        public PointStruct(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+    }
+    /// <summary>
+    /// A CLASS that represents a point
+    /// </summary>
+    public class PointClass
+    {
+
+        public int x;
+        public int y;
+
+        //constructor
+        public PointClass(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
